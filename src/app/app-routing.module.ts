73d179ec062392a3d0c91b7cab2ce7b6d8aeea72
Peter Balling <peter.balling@gmail.com>
2024-11-24 16:44:23 +0100

    Added basic menu

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OpencvComponent } from './opencv/opencv.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'opencv', component: OpencvComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
